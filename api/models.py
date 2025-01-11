591651dab4e727e83402d75467cca00e4ddc933f
Irfan Idriss Budaly <irfrs8@outlook.com>
2025-01-11 15:28:42 +0000

    added docstrings

--- api/models.py
+++ api/models.py
@@ -3,12 +3,19 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Hobby(models.Model):
+    """Represents a hobby for users to associate with on the platform"""
     name = models.CharField(max_length=100)
 
     def __str__(self):
+        """
+        Returns the hobby into its corresponding string representation
+        """
         return self.name
 
 class User(AbstractUser):
+    """
+    this will extend the default user model to include date of birth and hobbies
+    """
     dob = models.DateField(
         verbose_name="Date of Birth", null=True, blank=True
     )
@@ -16,7 +23,13 @@ class User(AbstractUser):
 
 
 class PageView(models.Model):
+    """
+    This is for keeping track of the number of page views
+    """
     count = models.IntegerField(default=0)
 
     def __str__(self):
+        """
+        For returning the page view function count as a string
+        """
         return f"Page view count: {self.count}"
